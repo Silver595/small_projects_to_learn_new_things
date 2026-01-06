6dab89e8af1d6072d34a6cfc40bf760010db9bf4
Dancing Snake <akashpurjalkar66@gmail.com>
2026-01-06 20:00:31 +0530

    socker for server

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
 import socket
+
+s = socket.socket()
+s.bind(('localhost', 3000))
+s.listen(1)
+
+print("Server is listening on port 3000...")
+
+conn,addr = s.accept()
+print(f"Connection from {addr} has been established!")
+
+while True:
+    data = conn.recv(1024).decode()
+    if not data:
+        break
+    print(f"Received: {data}")
+    conn.send(data.encode())
+
+conn.close()
